a7da76dec1ad553470e379f149d4643c44ebc804
Spiderssh <karurilawrence2019@gmail.com>
2024-12-17 09:53:59 +0300

    Create Example-RAT.java
    This code is for educational purposes only. Do not use it for malicious activities. Spiderssh is not responsible for your own actions.
    
    N.B part of this code is left out intentionally to avoid misuse.

---
+++ Example-RAT.java
@@ -0,0 +1,16 @@
+package com.example.rat;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Start the service
+        startService(new Intent(this, RatService.class));
+    }
+}
